c9ef733583a7b8134347160166d48eefb679e44e
Armaan 25 <armaan33000@gmail.com>
2024-11-25 04:56:48 +0000

    main

---
+++ src/home/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './homepage.css';
+
+const HomePage = () => {
+    return (
+        <>
+            <div className="homePage">
+                {/* man this takes a ton of time, at school rn :) */}
+                    <h3 className="text-3xl font-semibold text-white">This is a home page</h3>
+                </div>
+        </>
+    );
+
+}
+
+export default HomePage;
