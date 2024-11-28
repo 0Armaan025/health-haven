c3967e657725eeb7e1454ecd7c568d66823e90ad
Armaan <104704093+0Armaan025@users.noreply.github.com>
2024-11-28 10:29:28 +0530

    Update page.tsx

--- src/app/patients/page.tsx
+++ src/app/patients/page.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import './patientspage.css';
 import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/navbar/Footer";
 
 
 const PatientsPage = () => {
   return (
     <>
       <Navbar/>
-      <div className="patientsPage">
-      
+      <div className="patientsPage flex flex-col justify-center items-center">
+      <h3 style={{fontFamily: "Poppins, sans-serif"}} className="text-3xl font-semibold text-white">Patients Log</h3>
       
       </div>
+      <Footer/>
     </>
   );
 }
