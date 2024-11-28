63934d7bc0b2dd4edd87022ceb08b71e8e640402
Armaan <104704093+0Armaan025@users.noreply.github.com>
2024-11-28 10:38:58 +0530

    Update page.tsx

--- src/app/patients/page.tsx
+++ src/app/patients/page.tsx
@@ -3,14 +3,35 @@ import './patientspage.css';
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/navbar/Footer";
 
+type Patient = {
+  name: string;
+  timestamp: string;
+  room: string;
+};
 
 const PatientsPage = () => {
+  const [patient, setPatients] = useState<Patient[]>([
+    {
+      name: "Armaan",
+      timestamp: "10:45 AM",
+      room: "ROOM 102",
+    },
+    {
+      name: "Armaan 2",
+      timestamp: "10:45 AM",
+      room: "ROOM 104",
+    },
+  ]);
   return (
     <>
       <Navbar/>
       <div className="patientsPage flex flex-col justify-center items-center">
       <h3 style={{fontFamily: "Poppins, sans-serif"}} className="text-3xl font-semibold text-white">Patients Log</h3>
-      
+{/*       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {alerts.map((alert) => (
+            <AlertComponent key={alert.id} alert={alert} />
+          ))}
+        </div> */}
       </div>
       <Footer/>
     </>
