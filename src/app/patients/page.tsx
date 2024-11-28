f2cc2256f4f5290d5ed196eb6c7480befd9657af
Armaan <104704093+0Armaan025@users.noreply.github.com>
2024-11-28 10:10:41 +0530

    Create page.tsx

---
+++ src/app/patients/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './patientspage.css';
+
+const PatientsPage = () => {
+  return (
+    <>
+      <div className="patientsPage">
+      
+      
+      </div>
+    </>
+  );
+}
+
+export default PatientsPage;
