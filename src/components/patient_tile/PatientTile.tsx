c9501eda0ff361a31c60e97e93fba9b3feb146ad
Armaan <104704093+0Armaan025@users.noreply.github.com>
2024-11-28 10:31:00 +0530

    Create PatientTile.tsx

---
+++ src/components/patient_tile/PatientTile.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react';
+import './patienttile.css';
+
+const PatientTile = () => {
+  return (
+    <>
+      <div className="patientTile">
+      
+      </div>
+    </>
+  );
+}
+
+export default PatientTile;
